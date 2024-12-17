44d31e4407285033060814b318921662ac81c982
toutouff <63313022+toutouff@users.noreply.github.com>
2024-12-17 12:12:34 +0100

    feat: add websocket base support2

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from your_app.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
