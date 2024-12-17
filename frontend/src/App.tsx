cb2530f4cc78bbab9c94a21800a19555c62ae75c
PaulMazeau <mazeau.paul@gmail.com>
2024-12-17 12:22:16 +0100

    add : shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
+import { Button } from "@components/ui/button"
 
 function App() {
   return (
     <>
-      <div>
+      <div className="bg-red-500">
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
+        <Button>Click me</Button>
       </div>
     </>
   )
