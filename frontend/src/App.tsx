4e08f8102eb2594eb989c78a1053c5c595068d99
NicoooM <nmarsan33000@gmail.com>
2024-12-17 13:36:33 +0100

    feat: add leaflet map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {
+  Circle,
+  FeatureGroup,
+  LayerGroup,
+  MapContainer,
+  Popup,
+  Rectangle,
+  TileLayer,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_KEY;
+  const MAPBOX_STYLE_ID = "mapbox/streets-v11"; // Change this to your preferred map style
+
+  const center = [51.505, -0.09];
+  const rectangle = [
+    [51.49, -0.08],
+    [51.5, -0.06],
+  ];
+
+  const fillBlueOptions = { fillColor: "blue" };
+  const fillRedOptions = { fillColor: "red" };
+  const greenOptions = { color: "green", fillColor: "green" };
+  const purpleOptions = { color: "purple" };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <MapContainer
+          style={{
+            height: "100vh",
+            width: "100vw",
+          }}
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution="© Mapbox © OpenStreetMap contributors"
+            url={`https://api.mapbox.com/styles/v1/${MAPBOX_STYLE_ID}/tiles/{z}/{x}/{y}?access_token=${MAPBOX_ACCESS_TOKEN}`}
+          />
+          <LayerGroup>
+            <Circle
+              center={center}
+              pathOptions={fillBlueOptions}
+              radius={200}
+            />
+            <Circle
+              center={center}
+              pathOptions={fillRedOptions}
+              radius={100}
+              stroke={false}
+            />
+            <LayerGroup>
+              <Circle
+                center={[51.51, -0.08]}
+                pathOptions={greenOptions}
+                radius={100}
+              />
+            </LayerGroup>
+          </LayerGroup>
+          <FeatureGroup pathOptions={purpleOptions}>
+            <Popup>Popup in FeatureGroup</Popup>
+            <Circle center={[51.51, -0.06]} radius={200} />
+            <Rectangle bounds={rectangle} />
+          </FeatureGroup>
+        </MapContainer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
